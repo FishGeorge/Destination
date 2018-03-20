1376fedf5f91231d14f09ed38de66153e6f02dc1
FishGeorge <372804891@qq.com>
2018-03-21 00:04:13 +0800

    V0.1_InitialActivity
    好的，这就是0.1版本啦（我说它是它就是）。大家在分支里努力吧！

--- AndroidStudentClient/app/src/main/java/com/seuse16/destination/InitialActivity.java
+++ AndroidStudentClient/app/src/main/java/com/seuse16/destination/InitialActivity.java
@@ -1,7 +1,9 @@
 package com.seuse16.destination;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+//        // Hide UI
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
+//        }
+//        // Set the activity display in full screen
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
