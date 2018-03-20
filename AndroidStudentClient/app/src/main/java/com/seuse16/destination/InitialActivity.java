d7d16a883ca151c2eb8c4946e774b3f80430fce4
FishGeorge <372804891@qq.com>
2018-03-20 18:03:27 +0800

    启动界面
    logo得重新做，太糙了

---
+++ AndroidStudentClient/app/src/main/java/com/seuse16/destination/InitialActivity.java
@@ -0,0 +1,13 @@
+package com.seuse16.destination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
