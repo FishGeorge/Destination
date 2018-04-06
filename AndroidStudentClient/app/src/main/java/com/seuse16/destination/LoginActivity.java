f5cc359185309102484c8a71ff000148201dafc9
Sunwift-SEU <827121581@qq.com>
2018-04-06 16:42:47 +0800

    三个主界面的切换
    ==

--- AndroidStudentClient/app/src/main/java/com/seuse16/destination/LoginActivity.java
+++ AndroidStudentClient/app/src/main/java/com/seuse16/destination/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.seuse16.destination;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -27,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         final EditText account = findViewById(R.id.edit_account);
         final EditText password = findViewById(R.id.edit_password);
         TextView register = findViewById(R.id.login_register);
+        Button login = findViewById(R.id.btn_login);
         final CheckBox cb_password = findViewById(R.id.checkBox);
 
         // 密码明文密文设置
@@ -50,7 +53,14 @@ public class LoginActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            }
+        });
 
+        //假设密码用户名匹配，点击登陆进入主界面
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
             }
         });
     }
---
+++ AndroidStudentClient/app/src/main/java/com/seuse16/destination/Sun_f2.java
@@ -0,0 +1,20 @@
+package com.seuse16.destination;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Sun_f2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sun_f2, container, false);
+    }
+
+}
